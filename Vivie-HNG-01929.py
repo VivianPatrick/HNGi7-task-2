73b0003a0c229e060a2f6399bfbe03d6e3ac0629
Vivian Patrick <vivianpatrick456gmail.com>
2020-06-05 13:28:49 -0700

    Vivie-HNG-01929 script

--- Vivie-HNG-01929.py
+++ Vivie-HNG-01929.py
@@ -1,6 +1,6 @@
-Name = "[Vivian  Nzekwe]"
-Email = "shitta19@gmail.com"
-Id = "[HNG-0929]"
+Name = "Vivian Nzekwe"
+Email = "vivianpatrick458@gmail.com"
+Id = "HNG-01929"
 Language = "python"
 
 print("Hello World, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task" %(Name, Id, Email, Language))
